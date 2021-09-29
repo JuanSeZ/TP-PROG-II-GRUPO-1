a41e8dc61bb4dd4492a230925d19de1e70bc8d6e
JuanSeZ <91638993+JuanSeZ@users.noreply.github.com>
2021-09-29 19:48:52 -0300

    Add files via upload

---
+++ Ej_lampara_1_Clase.py
@@ -0,0 +1,30 @@
+class Lampara:
+    def __init__(self,marca):
+        self.foco = False
+        self.marca = marca
+
+    def getFoco(self):
+        return self.foco
+
+    def getMarca(self):
+        return self.marca
+
+    def prender(self):
+        if self.foco == False:
+            self.foco = True
+
+    def apagar(self):
+        if self.foco == True:
+            self.foco = False
+
+marca = 'Phillips'
+lampara_1_living = Lampara(marca)
+print('la marca del foco es: ',lampara_1_living.getMarca())
+lampara_1_living.prender()
+#lampara_1_living.apagar()
+
+
+if lampara_1_living.getFoco() == True:
+    print('Foco encendido')
+else:
+    print('Foco Apagado')
