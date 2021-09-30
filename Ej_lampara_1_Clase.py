60ad72e200f4d72fef91118c1b1f5aae9432daa5
Parrandos <63514959+Parrandos@users.noreply.github.com>
2021-09-29 21:54:24 -0300

    prueba

--- Ej_lampara_1_Clase.py
+++ Ej_lampara_1_Clase.py
@@ -22,7 +22,7 @@ lampara_1_living = Lampara(marca)
 print('la marca del foco es: ',lampara_1_living.getMarca())
 lampara_1_living.prender()
 #lampara_1_living.apagar()
-
+print("Hola")
 
 if lampara_1_living.getFoco() == True:
     print('Foco encendido')
