b7ae029b141a50120517e842c8af3ea88f90636c
MatiBrego <91639101+MatiBrego@users.noreply.github.com>
2021-10-09 22:31:22 -0300

    Add files via upload
    Módulo User con las clases user, citizen y admin. Los metodos de cada clase no están implementados y llevan la descripción de lo que deben hacer.

---
+++ User.py
@@ -0,0 +1,46 @@
+from abc import ABC
+
+class user(ABC):
+    def __init__(self, cuil, cellphone):
+        self.cuil = cuil
+        self.cellphone = cellphone
+
+    def report_event(self, type):
+        #Debe crear una instancia de la clase 'evento', con un tipo especifico
+        pass
+
+class admin(user):
+    def ban(self, citizen):
+        #Debe prohibir el acceso a su cuenta al ciudadano
+        pass
+
+    def unban(self, citizen):
+        #Debe rehabilitar el acceso a su cuenta al ciudadano
+        pass
+
+    def create_event_type(self, type):
+        #Debe crear una instancia de la clase 'tipo_de_evento'
+        pass
+
+    def create_sensor(self, type):
+        #Debe crear una instancia de la clase 'sensor', con un tipo de evento asignado
+        pass
+
+class citizen(user):
+    def __init__(self, cuil, cellphone):
+        super.__init__(cuil, cellphone)
+        friend_list = []
+        friend_request_list = []
+        strikes = 0 #Cantidad de solicitudes rechazadas por otro ciudadanos
+
+    def check_friend_requests(self):
+        #Debe devolver(o mostrar por pantalla) la lista de de solicitudes de amistad
+        pass
+
+    def send_friend_request(self, citizen):
+        #Debe enviarle una solicitud de amistad a otro ciudadano
+        pass
+
+    def reject_friend_request(self, citizen):
+        #Debe quitar de la lista de solicitudes al ciudadano y agregarle un strike
+        pass
