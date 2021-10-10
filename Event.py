107a29d8df28ef5ba576d506e47d98ac1262820e
MatiBrego <91639101+MatiBrego@users.noreply.github.com>
2021-10-10 19:33:08 -0300

    Add files via upload

---
+++ Event.py
@@ -0,0 +1,4 @@
+class event:
+    def __init__(self, type, coordinates):
+        self.type = type #Debe ser una instancia de la clase 'event_type'
+        self.coordinates = coordinates
---
+++ Event_type.py
@@ -0,0 +1,4 @@
+class event_type:
+    def __init__(self, description):
+        self.description = description
+        self.ocurrence_list = [] #Lista con instancias de la clase 'evento', con el mismo tipo que la instancia de esta clase
