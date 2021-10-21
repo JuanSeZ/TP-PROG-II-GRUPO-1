43325fc11b267afb7668f6cadea66fcce2b99613
maxigeist <91639094+maxigeist@users.noreply.github.com>
2021-10-20 22:48:14 -0300

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -1,1 +1,7 @@
-#Inventar nombres con un numero de telefono y cuil
+#Dataset ficticio de ANSES que verifica si los cuidadanos se registran con datos veridicos
+Juan Perez: 145, 9432
+Miguel Rulo: 390, 0012
+Lucas Nelo: 245, 7003
+Nilda Russo: 590, 6549
+Miriam Orne: 220, 1122
+Juliana Aguo: 798, 0139
