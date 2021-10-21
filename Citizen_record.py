b5b80d21e0e88116a86f513d551c2be3cb70c1f0
maxigeist <91639094+maxigeist@users.noreply.github.com>
2021-10-21 13:43:21 -0300

    Update Citizen_record.py

--- Citizen_record.py
+++ Citizen_record.py
@@ -1,6 +1,31 @@
+from User import user, citizen
+import csv
+import unittest
+
 class citizen_record:
     citizen_list = [] #Lista de ciudadanos registrados
 
+    
+    
+    #Usando la biblioteca csv, se pueden autenticar los datos presentes en el dataset de anses y compararlos con los datos ingresados por el ciudadano a la hora de registrarse.
+    @staticmethod
     def register_citizen(citizen):
         #Debe agregar un ciudadano a la lista de ciudadanos registrados
-        __class__.citizen_list.append(citizen)
+        with open ("Dataset.csv") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if ("{0}".format(row[2])) == str(citizen.cuil) and ("{0}".format(row[1])) == str(citizen.cellphone):
+                    __class__.citizen_list.append(citizen)
+
+    @staticmethod
+    def get_citizen_list():
+        return __class__.citizen_list
+
+#Test que comprueba la validacion del usuario mediante el DATASET
+class test_user(unittest.TestCase):
+
+    def test_dataset(self):
+        Juan = citizen(9432, 145)
+        citizen_record.register_citizen(Juan)
+        self.assertEqual(citizen_record.citizen_list, [Juan])
+
