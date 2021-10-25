40c891a5fe603b27d6257271abd96e163b5a7cab
MatiBrego <91639101+MatiBrego@users.noreply.github.com>
2021-10-25 17:23:28 -0300

    Add files via upload

--- Citizen_record.py
+++ Citizen_record.py
@@ -1,32 +1,38 @@
-from User import user, citizen
+from User import citizen
 import csv
 import unittest
 
 class citizen_record:
     citizen_list = [] #Lista de ciudadanos registrados
 
-    
-    
-    #Usando la biblioteca csv, se pueden autenticar los datos presentes en el dataset de anses y compararlos con los datos ingresados por el ciudadano a la hora de registrarse.
-    @staticmethod
-    def register_citizen(citizen):
+    def register_citizen(user_Cuil, user_password, user_number):
         #Debe agregar un ciudadano a la lista de ciudadanos registrados
+        new_citizen = citizen(user_Cuil, user_number) #Hay que agregar el password
+        __class__.citizen_list.append(new_citizen)
+
+    def validate_registration(Cuil, phone_number):
+        #Debe ver si los datos ingresados concuerdan con datos en el Dataset de Anses
         with open ("Dataset.csv") as f:
             reader = csv.reader(f)
             for row in reader:
                 #La primer parte del if, compara con el cuil, la segunda parte del if, compara con el teléfono.
-                if ("{0}".format(row[2])) == str(citizen.cuil) and ("{0}".format(row[1])) == str(citizen.cellphone):
-                    __class__.citizen_list.append(citizen)
+                if ("{0}".format(row[2])) == str(Cuil) and ("{0}".format(row[1])) == str(phone_number):
+                    return True
+                else:
+                    return False
 
-    @staticmethod
+    def validate_login():
+        #Debe ver si los datos ingresados concuerdan con algun ciudadano de citizen_list[]
+        pass
+
     def get_citizen_list():
         return __class__.citizen_list
 
-#Test que comprueba la validacion del usuario mediante el DATASET
 class test_user(unittest.TestCase):
 
     def test_dataset(self):
-        Juan = citizen(9432, 145)
-        citizen_record.register_citizen(Juan)
-        self.assertEqual(citizen_record.citizen_list, [Juan])
+        citizen_record.register_citizen(9432, 'hola', 145)
+        self.assertEqual(citizen_record.citizen_list[0].get_cuil(), 9432)
 
+if __name__ == '__main__':
+    unittest.main()
--- menu.py
+++ menu.py
@@ -1,5 +1,4 @@
 from Citizen_record import citizen_record
-from User import citizen, admin
 
 
 class main_menu():
@@ -12,24 +11,43 @@ class main_menu():
             if user_input == '1':
                 __class__.enter_login_info()
             elif user_input == '2':
-                __class__.register()
-    
+                __class__.enter_register_info()
+
     def enter_login_info():
         user_input = ''
         while user_input != 'exit':
             user_Cuil = int(input('Enter Cuil: ')) 
             user_password = input('Enter password: ')
-            for user in citizen_record.get_citizen_list():
-                if user.get_cuil() == user_Cuil:
-                    if user.get_password() == user_password:
-                        __class__.login()
-                        print('\nReturning to Main Menu...\n')
-                        return ' '
+            if citizen_record.validate_login(user_Cuil, user_password): #Hay que implementar el metodo validate_login()
+                __class__.login()
+                print('\nReturning to Main Menu...\n')
+                return ' '
             
             print("\n\tYour Cuil or password were incorrect. \n\tIf you want to try again, press enter.\n\tIf you want to leave to the Main Menu, type 'exit'\n")
             user_input = input("Press enter or type 'exit': ")
         
+    def enter_register_info():
+        user_input = ''
+        while user_input != 'exit':
+            user_Cuil = int(input('Enter Cuil: '))
+            user_number = int(input('Enter phone number: '))
+            user_password = input('Enter password: ')
+            user_password_validation = input('Enter password again: ')
+
+            if citizen_record.validate_registration(user_Cuil, user_number) and user_password == user_password_validation: #Hay que implementar el metodo validate_registration()
+                __class__.register()
+                print('\nReturning to Main Menu...\n')
+                return ' '
             
+            print("\n\tSomething went wrong.\n\tBe sure to put your Cuil, phone number and passwords(both times) correctly \n\tIf you want to try again, press enter.\n\tIf you want to leave to the Main Menu, type 'exit'\n")
+            user_input = input("Press enter or type 'exit': ")
     
     def login():
-        print("\nLogin succefull!\n")
+        print("\nLogin was succefull!\n")
+
+    def register(user_Cuil, user_password, user_number):
+        print("\nRegistration was succefull!")
+        citizen_record.register_citizen(user_Cuil, user_password, user_number)
+
+
+main_menu.launch_main()
