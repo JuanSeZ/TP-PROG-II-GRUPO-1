d85aa8873467c0e96c2de26a11c6020d49a2ae70
Parrandos <63514959+Parrandos@users.noreply.github.com>
2021-11-15 14:18:20 -0300

    .

--- Event_type.py
+++ Event_type.py
@@ -2,15 +2,15 @@ class Event_type:
 
     def __init__(self, description):
         self.description = description
-        self.ocurrence_list = [] #Lista con instancias de la clase 'evento', con el mismo tipo que la instancia de esta clase
+        self.event_list = [] #Lista con instancias de la clase 'evento', con el mismo tipo que la instancia de esta clase
 
     def __repr__(self):
         return self.description
 
     def get_ocurrence_list(self):
-         return self.ocurrence_list
+         return self.event_list
 
 
     def add_ocurrence(self, ocurrence):
         #Agrega una ocurrencia del tipo de evento
-        self.ocurrence_list.append(ocurrence)
+        self.event_list.append(ocurrence)
